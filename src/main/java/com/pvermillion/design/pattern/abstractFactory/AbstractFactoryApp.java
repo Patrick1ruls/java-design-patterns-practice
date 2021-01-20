6a169f7642b56898920e4dfbcbbf6135c62ad5c4
Patrick1ruls <patrickvermillion555@gmail.com>
2021-01-19 21:17:09 -0600

    :pencil: Note the purpose of the abstract factory design pattern

--- src/main/java/com/pvermillion/design/pattern/abstractFactory/AbstractFactoryApp.java
+++ src/main/java/com/pvermillion/design/pattern/abstractFactory/AbstractFactoryApp.java
@@ -2,6 +2,10 @@ package com.pvermillion.design.pattern.abstractFactory;
 
 import com.pvermillion.basic.Basic;
 
+/**
+ * Abstract Factory is a very central design pattern for Dependency Injection (DI).
+ */
+
 public class AbstractFactoryApp {
     public static void main(String[] args) {
         Basic.getLogger().info("Hello World!");
